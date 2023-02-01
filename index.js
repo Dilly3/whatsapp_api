f61072977ea2dae73e5c1eea9d4f7aaffc30b3e9
michael anikamadu <dil.anikamadu@gmail.com>
2023-02-01 06:47:45 +0100

    indexjs.v1

---
+++ index.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+const express = require("express");
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN
+const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN
+const PHONE_NO_ID = process.env.PHONE_NO_ID
