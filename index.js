7db70bb7656b9683fbaaf8b2ee458d7b4cbfb529
michael anikamadu <dil.anikamadu@gmail.com>
2023-02-01 06:53:11 +0100

    indexjs.v2

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 require('dotenv').config();
+var bodyParser = require('body-parser')
+
+
+
 const express = require("express");
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN
 const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN
 const PHONE_NO_ID = process.env.PHONE_NO_ID
+const PORT = process.env.PORT || 8000;
+
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter()
+emitter.setMaxListeners(60)
+const app = express();
+app.use(express.json(),bodyParser.urlencoded({extended:false}))
+
+
+app.listen(PORT, ()=>{console.log(`listening on port: ${PORT}`)})
