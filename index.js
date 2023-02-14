a8741f9d94d8794b636f16a5a0f1ea3c85ecdb04
michael anikamadu <dil.anikamadu@gmail.com>
2023-02-14 23:26:11 +0100

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {format} = require('date-fns')
 const express = require("express");
 const bodyParser = require('body-parser')
 const EventEmitter = require('events');
+const {colref, msgRef, sellerRef} = require('./database/config')
 const {sendReply,unpackRequestBody} = require('./utility/helperfunc')
 
 
@@ -50,16 +51,16 @@ app.post('/webhook', (req, res) => {
         .then(data => {
             const { sender_number, sender_name, body1:message_body, timestamp1:timestamp } = data
             message_body1 = message_body
-            msg_store.addDocument({sender_name, phone_number:sender_number, message_body:message_body,timestamp: `${new Date()}`})
+            msg_store.addDocument_whasapp({sender_name, phone_number:sender_number, message_body:message_body,timestamp: new Date()})
             .then((data)=>{
-            console.log(data)
+            
             })
             .catch((err)=>{console.log(err)})
-            
-            return sendReply(PHONE_NO_ID, WHATSAPP_TOKEN, sender_number, sender_name, message_body)
+           // console.log(sender_number)
+            return sendReply(PHONE_NO_ID, WHATSAPP_TOKEN, sender_number, sender_name, false, message_body)
         }).then((response => {
-            const { messages,contacts } = response.data
-            
+           // const { messages,contacts } = response
+            //console.log(response)
             
             res.json({ 'messageID': `${messages[0].id}`, sender_phone_number : `${contacts[0].wa_id}`, "message_body" : `${message_body1}` })
         })
@@ -70,7 +71,46 @@ app.post('/webhook', (req, res) => {
 
     }
 })
-msg_store.getDocuments().then(console.log)
+
+let docid = ''
+
+
+msgRef.orderBy('updatedAt','asc').onSnapshot((snapshot)=>{
+    msg_store.onChanges(snapshot).then(data=>{
+        if(data.sellerInfo){
+            
+            const{docid} = data.sellerInfo
+            console.log(docid)
+            const {phoneNumber,firstName,sellerUnread} = data.sellerInfo.sellerInfo
+            
+            //console.log("index.js ln 78: \n",phoneNumber)
+           //console.log(data.data)   
+         
+          sendReply(PHONE_NO_ID, WHATSAPP_TOKEN, phoneNumber, firstName,true) 
+         msg_store.msgRef.doc(docid).set({
+            sellerUnread:0
+        },{merge:true}) 
+        }else{
+            
+        }
+        
+    }).catch(err=>console.log(err))
+})
+
+
+
+const message = {
+    updatedAt: new Date(),
+    sellerAvatar: "",
+    buyerId: "eff12345678982",
+    buyerAvatar:"",
+    sellerUnread: 2,
+    sellerId: "DrLy4UmACRUIPn9ALYD7sL3YmDt2",
+    sellerName: "xabac Richie",
+    buyerName: 'jacob dean3000'
+}
+//msg_store.addDocument_msg(message)
+//msg_store.removeDocument('6b3e1186-745d-4c5d-bb2f-8a6f58ea793c')
 
 app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`)
