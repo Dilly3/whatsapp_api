71ce92abb2e1cc2be612c470546b796b8218f746
michael anikamadu <dil.anikamadu@gmail.com>
2023-02-15 09:15:13 +0100

    update

--- database/config.js
+++ database/config.js
@@ -1,14 +1,14 @@
-serviceAccount = require('./cashaam-dev-firebase-adminsdk-wvlo9-a16bfe900c.json')
+serviceAccount = require('./admin.json')
 const admin = require('firebase-admin')
 const { initializeApp } = require('firebase-admin/app');
 
 require('dotenv').config();
 
 
-  
+
 const projectId = process.env.PROJECT_ID;
 initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: `https://${projectId}.firebaseio.com`, // check it under service accounts
   storageBucket: `${projectId}.appspot.com`, //check it in script snippet require to add to your website.
 })
@@ -16,7 +16,7 @@ const WHATSAPP_MSG = process.env.WHATSAPP_MSG
 const MESSAGE = process.env.MESSAGES
 const SEL_ACCT = process.env.SELL_ACCOUNTS
 const db = admin.firestore()
-db.settings({timestampsInSnapshots:true})
+db.settings({ timestampsInSnapshots: true })
 const colref = db.collection(WHATSAPP_MSG)
 const msgRef = db.collection(MESSAGE)
 const sellerRef = db.collection(SEL_ACCT)
@@ -26,4 +26,4 @@ const sellerRef = db.collection(SEL_ACCT)
 
 
 
-module.exports = {colref,msgRef,sellerRef}
+module.exports = { colref, msgRef, sellerRef }
